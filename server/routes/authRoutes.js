8dae812ddefc5c815da4468bb93d0e8a37d63c52
Abhishek Mishra <cs.abhishekmo@gmail.com>
2025-01-18 16:12:10 +0530

    minor fix

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,10 +4,10 @@ const { verifyEmail } = require('../controllers/authEmailController')
 
 const router = express.Router();
 
+router.get('/verify', verifyEmail);
 router.post('/register', register);
 router.post('/register-club', registerClub);
 router.post('/login', login);
-router.get('/verify', verifyEmail);
 
 
 module.exports = router;
